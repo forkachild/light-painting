5aa3bd76807dc1302306d29038796969abf31c60
Suhel Chakraborty <chakraborty.suhel@gmail.com>
2023-01-14 23:49:16 +0530

    Config extracted

--- include/fft.h
+++ include/fft.h
@@ -9,10 +9,12 @@ void fill_twiddles(float complex *twiddles, uint N);
 
 void fill_twiddles_d(double complex *twiddles, uint N);
 
-void fft_dit_rad2(float complex *X, const float complex *twiddles,
+void fft_rad2_dit(float complex *X, const float complex *twiddles,
                   const uint *reversed_indices, uint N);
 
-void fft_dit_rad2_d(double complex *X, const double complex *twiddles,
+void fft_rad2_dit_d(double complex *X, const double complex *twiddles,
                     const uint *reversed_indices, uint N);
 
-void fft_dif_rad2(float complex *X, const float complex *twiddles, uint N);
+void fft_rad2_dif(float complex *X, const float complex *twiddles, uint N);
+
+void fft_rad2_dif_d(double complex *X, const double complex *twiddles, uint N);
--- program.c
+++ program.c
@@ -13,6 +13,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SPECTRUM_COVERAGE 0.3f
+#define ATTENUATE_FACTOR 6
+#define OUTPUT_SCALE 5.f
+
 #define SAMPLE_COUNT 64
 #define LED_COUNT 300
 #define AUDIO_INPUT_MAX_AMP (((int32_t)1 << 23) - 1)
@@ -30,62 +34,7 @@ static float *envelope = NULL;
 static uint *reversed_indices = NULL;
 static Canvas canvas;
 
-const float sample_per_led = (0.3f * FREQ_BIN_COUNT) / LED_COUNT;
-
-static inline void cache_envelope() {
-    envelope = malloc(SAMPLE_COUNT * sizeof(float));
-
-    double angle_per_sample = M_PI / SAMPLE_COUNT;
-    for (uint i = 0; i < SAMPLE_COUNT; i++)
-        envelope[i] = sin(angle_per_sample * i);
-}
-
-static inline void cache_twiddles() {
-    twiddles = malloc(FREQ_BIN_COUNT * sizeof(float complex));
-    fill_twiddles(twiddles, SAMPLE_COUNT);
-}
-
-static inline void cache_reversed_indices() {
-    reversed_indices = malloc(SAMPLE_COUNT * sizeof(uint));
-    fill_reversed_indices(reversed_indices, SAMPLE_COUNT);
-}
-
-static inline void setup_gpio() {
-    gpio_init(LR_PIN);
-    gpio_set_dir(LR_PIN, true);
-    gpio_set_pulls(LR_PIN, false, true);
-}
-
-static inline void setup_drivers() {
-    if (inmp441_init(SAMPLE_COUNT, SCK_PIN, WS_PIN, DATA_PIN) !=
-        RESULT_ALL_OK) {
-        printf("Audio init failed\n");
-    }
-
-    if (ws2812_init(LED_COUNT, LED_PIN) != RESULT_ALL_OK) {
-        printf("LED Driver init failed\n");
-    }
-
-    if (canvas_init(&canvas, LED_COUNT) != RESULT_ALL_OK) {
-        printf("Canvas init failed\n");
-    }
-}
-
-static inline void setup() {
-    // stdio_init_all();
-
-    setup_gpio();
-    setup_drivers();
-
-    samples = malloc(SAMPLE_COUNT * sizeof(float complex));
-    if (!samples) {
-        printf("FFT Samples alloc failed\n");
-    }
-
-    cache_twiddles();
-    cache_reversed_indices();
-    cache_envelope();
-}
+const float sample_per_led = (SPECTRUM_COVERAGE * FREQ_BIN_COUNT) / LED_COUNT;
 
 static inline float get_abs_freq_bin(uint i) {
     return cabsf(samples[reversed_indices[i]]) / FREQ_BIN_COUNT;
@@ -146,13 +95,65 @@ static inline GRBAColor hsv_to_rgb(uint8_t h, uint8_t s, uint8_t v) {
 }
 
 static inline GRBAColor led_get_color(uint i) {
-    float freq_bin = get_abs_freq_bin((sample_per_led * i) + 1) * 5.f;
+    float freq_bin = get_abs_freq_bin((sample_per_led * i) + 1) * OUTPUT_SCALE;
     if (freq_bin > 1.f)
         freq_bin = 1.f;
 
     uint8_t value = freq_bin * 0xFF;
-
     return hsv_to_rgb(value, 0xFF, value);
+}
+
+static inline void setup_gpio() {
+    gpio_init(LR_PIN);
+    gpio_set_dir(LR_PIN, true);
+    gpio_set_pulls(LR_PIN, false, true);
+}
+
+static inline void setup_drivers() {
+    if (inmp441_init(SAMPLE_COUNT, SCK_PIN, WS_PIN, DATA_PIN) !=
+        RESULT_ALL_OK) {
+        printf("Audio init failed\n");
+    }
+
+    if (ws2812_init(LED_COUNT, LED_PIN) != RESULT_ALL_OK) {
+        printf("LED Driver init failed\n");
+    }
+
+    if (canvas_init(&canvas, LED_COUNT) != RESULT_ALL_OK) {
+        printf("Canvas init failed\n");
+    }
+}
+
+static inline void cache_envelope() {
+    envelope = malloc(SAMPLE_COUNT * sizeof(float));
+
+    double angle_per_sample = M_PI / SAMPLE_COUNT;
+    for (uint i = 0; i < SAMPLE_COUNT; i++)
+        envelope[i] = sin(angle_per_sample * i);
+}
+
+static inline void cache_twiddles() {
+    twiddles = malloc(FREQ_BIN_COUNT * sizeof(float complex));
+    fill_twiddles(twiddles, SAMPLE_COUNT);
+}
+
+static inline void cache_reversed_indices() {
+    reversed_indices = malloc(SAMPLE_COUNT * sizeof(uint));
+    fill_reversed_indices(reversed_indices, SAMPLE_COUNT);
+}
+
+static inline void setup_buffers() {
+    samples = malloc(SAMPLE_COUNT * sizeof(float complex));
+
+    cache_twiddles();
+    cache_reversed_indices();
+    cache_envelope();
+}
+
+static inline void setup() {
+    setup_gpio();
+    setup_drivers();
+    setup_buffers();
 }
 
 int main() {
@@ -181,15 +182,16 @@ int main() {
             (int32_t *)swapchain_node_get_buffer_ptr(node);
 
         for (uint i = 0; i < SAMPLE_COUNT; i++) {
-            samples[i] = (float)(int_samples[i] >> 8) / AUDIO_INPUT_MAX_AMP;
-            samples[i] *= envelope[i];
+            int32_t sample = int_samples[i] & 0xFFFFFF00;
+            samples[i] = (sample >> ATTENUATE_FACTOR) * envelope[i] /
+                         AUDIO_INPUT_MAX_AMP;
         }
 
         saved_irq = save_and_disable_interrupts();
         swapchain_return_after_read(audio_swapchain, node);
         restore_interrupts(saved_irq);
 
-        fft_dif_rad2(samples, twiddles, SAMPLE_COUNT);
+        fft_rad2_dif(samples, twiddles, SAMPLE_COUNT);
 
         for (uint i = 1; i < LED_COUNT; i++)
             canvas_point(&canvas, i, led_get_color(i));
